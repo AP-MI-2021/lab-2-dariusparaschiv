809c98e07a525b19980a815161c9f16b30af8516
dariusparaschiv <91637941+dariusparaschiv@users.noreply.github.com>
2021-10-25 19:37:51 +0300

    lab2

--- labo2.py
+++ labo2.py
@@ -1,5 +1,13 @@
 import math
+'''
+    def function get_base_2(p:str):
+    cuv = ''
+    while p != '':
+        cuv = cuv + str(int(p % 2))
+        p = p[:-1]
+    return cuv
 
+'''
 
 def get_n_choose_k(n: int, k: int):
     t = (math.factorial(n) // (math.factorial(k) * math.factorial(n - k)))
@@ -13,6 +21,42 @@ def is_palindrome(n):
         t = t * 10 + aux % 10
         aux = aux // 10
     return t == n
+
+
+def is_prime(x):
+    if x < 2:
+        return False
+    else:
+        for i in range(2, x // 2 + 1):
+            if x % i == 0:
+                return False
+    return True
+
+
+def get_largest_prime_below(n):
+    n = n-1
+    if n<2:
+        return 0
+    else:
+        while n>1:
+            if is_prime(n):
+                return n
+            else:
+                n = n-1
+    return 0
+
+
+def test_get_largest_prime_below():
+    assert get_largest_prime_below(2) == 0
+    assert get_largest_prime_below(3) == 2
+    assert get_largest_prime_below(9) == 7
+
+
+def test_is_prime():
+    assert is_prime(1) is False
+    assert is_prime(4) is False
+    assert is_prime(2) is True
+    assert is_prime(19) is True
 
 
 def test_get_n_choose_k():
@@ -30,9 +74,12 @@ def test_is_palindrome():
 def main():
     test_get_n_choose_k()
     test_is_palindrome()
+    test_is_prime()
+    test_get_largest_prime_below()
     while True:
         print('1.Calculează combinări de `n` luate câte `k`')
         print('2.Verifica daca un nr e palindrom ')
+        print('3.Cel mai mare nr prim, mai mic decat n dat')
         print('x. Iesire')
         optiune = input('Alegeti optiunea ')
         if optiune == '1':
@@ -43,8 +90,15 @@ def main():
             else:
                 print('nu se poate')
         elif optiune == '2':
-            n = int(input('Scrieti un nr'))
+            n = int(input('Scrieti un nr '))
             print(is_palindrome(n))
+        elif optiune == '3':
+            n = int(input('Scrieti un nr '))
+            t = get_largest_prime_below(n)
+            if t == 0:
+                print("nu exista")
+            else:
+                print(t)
         elif optiune == 'x':
             break
         else:
